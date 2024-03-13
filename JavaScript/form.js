8a1a1c7929dc82762ef951c5feb4d702011cbc02
Ivan Negohe <inegohe@student.refactory.academy>
2024-03-13 12:19:45 +0300

    update the project css layout

--- JavaScript/form.js
+++ JavaScript/form.js
@@ -12,6 +12,15 @@ function validateForm(){
     return true;
 }
 
+//fist check for the name input, if the name input is okay go on to check for the email input, but if its not okay send out a message inform the use of it.
+
+//secondly if the name is fine check for the email, if the email is okay go on to check for the password, but if the email is not okay send out a message to the user.
+
+//thirdly if the pasword is fine inform the user of submission, but if its not fine then send out a mesaeg to the user.
+
+
+
+
 //function for checking the name field if its not empty
 function nameCheck(x){
     if (x === "" || x === null){
@@ -21,8 +30,11 @@ function nameCheck(x){
         return true;
     }
 }
+
 // function for checking the email field if its correct
+
 function emailCheck(input){
+
     //we create a patern variable 
     let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if(input.match(validRegex)){
